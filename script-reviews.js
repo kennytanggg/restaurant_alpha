8b49e9eb83584da3ce10d24b668a589bc1743fad
Kenny Tanglao <ktanglao@gmail.com>
2021-01-10 20:33:15 -0500

    Menu => Reviews (practicing mapping data from an API) NEXT: finish styling

---
+++ script-reviews.js
@@ -0,0 +1,35 @@
+const location_card = document.getElementById('location-card');
+
+const url = 'https://developers.zomato.com/api/v2.1/reviews?res_id=16781875&start=1&count=5';
+const userKey = 'd477e4b5e5c20788dada4c06d202a840';
+const res_id = '17788817';
+
+const restaurant = fetch(url, {
+    headers: {
+        Accept: "application/json",
+        "User-Key": "d477e4b5e5c20788dada4c06d202a840"
+    }
+    // }).then(response => console.log(response.json())
+}).then(response => {
+    return response.json();
+}).then(data => {
+    let reviews = data.user_reviews;
+    reviews.forEach((review, index) => {
+        const review_card = createReview(review.review, index);
+        document.querySelector('.cards').appendChild(review_card);
+    });
+});
+
+
+function createReview(review, index) {
+    const review_card = document.createElement('div');
+    review_card.className = `card-${index}`;
+    review_card.innerHTML = `
+        <p>Rating: ${review.rating_text}</p>
+        <p>Likes: ${review.likes}</p>
+        <p>Author: ${review.user.name}</p>
+        <p>Review: ${review.rating_text}</p>
+        <p>${review.review_time_friendly}</p>
+    `;
+    return review_card;
+}
